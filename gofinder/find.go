a8b020a5fb0d8e9cfc95b1bf3e7c2f525b8bd1c1
Kevin Kirsche <Kev.Kirsche@gmail.com>
2017-10-24 14:01:29 -0400

    Bug fix — and not or

--- gofinder/find.go
+++ gofinder/find.go
@@ -19,7 +19,7 @@ func (g *GoFinder) Find(resp *http.Response) error {
 	eltype := "unknown"
 	found := false
 
-	if !g.config.Comments || !g.config.Links || !g.config.Scripts || !g.config.Title {
+	if !g.config.Comments && !g.config.Links && !g.config.Scripts && !g.config.Title {
 		found = true
 		logrus.Warnln("Not searching for any items...")
 	}
